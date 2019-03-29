1e1814f300fa45b8b377b95f68c32c77b5f16e2b
Dmitry <berezkin.ds@gmail.com>
2019-03-29 15:59:09 +0300

    [ADD] - koin scope usage

--- app/src/main/java/net/android/diframeworks/di/koin/koin_module.kt
+++ app/src/main/java/net/android/diframeworks/di/koin/koin_module.kt
@@ -1,13 +1,15 @@
 package net.android.diframeworks.di.koin
 
 import net.android.diframeworks.view.koin.KoinVM
-import net.android.diframeworks.view.main.MainVM
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val koinAppModule = module {
     viewModel { KoinVM() }
 //    viewModel { MainVM() }
+    scope("koinScopedObject") { KoinScopedObject() }
 }
 
 val koinModules = listOf(koinAppModule)
+
+class KoinScopedObject
--- app/src/main/java/net/android/diframeworks/view/koin/KoinFragment.kt
+++ app/src/main/java/net/android/diframeworks/view/koin/KoinFragment.kt
@@ -4,16 +4,27 @@ import android.os.Bundle
 import android.view.View
 import net.android.diframeworks.R
 import net.android.diframeworks.databinding.FragmentKoinBinding
+import net.android.diframeworks.di.koin.KoinScopedObject
+import net.android.diframeworks.util.logInfo
 import net.android.diframeworks.view.BaseFragment
+import org.koin.android.ext.android.inject
+import org.koin.androidx.scope.ext.android.bindScope
+import org.koin.androidx.scope.ext.android.getOrCreateScope
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class KoinFragment : BaseFragment<KoinVM, FragmentKoinBinding>() {
     override val vm: KoinVM by viewModel()
 
+    private val koinScopedObject1: KoinScopedObject by inject()
+    private val koinScopedObject2: KoinScopedObject by inject()
+
     override fun getLayout(): Int = R.layout.fragment_koin
 
     override fun afterCreateView(view: View, savedInstanceState: Bundle?) {
+        bindScope(getOrCreateScope("koinScopedObject"))
         vb = FragmentKoinBinding.bind(view)
         vm
+        logInfo("koinScopedObject1: $koinScopedObject1")
+        logInfo("koinScopedObject2: $koinScopedObject2")
     }
 }
